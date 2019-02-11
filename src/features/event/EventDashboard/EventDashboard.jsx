0cbe44561fc19d65159df1e6ca3cfc3144df17c7
siddarthmadan <siddarthmadank@gmail.com>
2019-02-10 21:01:36 -0700

    Added infinite scroll to events page

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,22 +1,56 @@
 import React, { Component } from 'react'
 import {connect } from 'react-redux'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Loader } from 'semantic-ui-react'
 import EventList from '../eventList/EventList'
 import { cancelToggle} from '../eventActions';
 import LoadingComponent from '../../../app/layout/LoadingComponent'
 import EventActivity from "../EventActivity/EventActivity";
 import {firestoreConnect} from 'react-redux-firebase'
+import { getEventsForDashboard } from '../eventActions';
 
-const mapState = (state) => ({
-  events : state.firestore.ordered.events,
-  loading: state.async.loading
-})
+const query = [
+  {
+    collection: 'activity',
+    orderBy: ['timestamp', 'desc'],
+    limit: 5
+  }
+];
+
+const mapState = state => ({
+  events: state.events,
+  loading: state.async.loading,
+  activities: state.firestore.ordered.activity
+});
 
 const actions = {
-  cancelToggle
-}
+  getEventsForDashboard
+};
 
 class EventDashboard extends Component {
+  state = {
+    moreEvents: false,
+    loadingInitial: true,
+    loadedEvents: [],
+    contextRef: {}
+  };
+
+  async componentDidMount() {
+    let next = await this.props.getEventsForDashboard();
+    if (next && next.docs && next.docs.length > 1) {
+      this.setState({
+        moreEvents: true,
+        loadingInitial: false
+      });
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.events !== nextProps.events) {
+      this.setState({
+        loadedEvents: [...this.state.loadedEvents, ...nextProps.events]
+      });
+    }
+  }
 
   handleDeleteEvent = deletedEventId => () => {
     this.props.cancelToggle(deletedEventId);
@@ -41,20 +75,45 @@ class EventDashboard extends Component {
       isOpen:true
     })
   }
+  
+  getNextEvents = async () => {
+    const { events } = this.props;
+    let lastEvent = events && events[events.length - 1];
+    let next = await this.props.getEventsForDashboard(lastEvent);
+    if (next && next.docs && next.docs.length <= 1) {
+      this.setState({
+        moreEvents: false
+      });
+    }
+  };
 
+  handleContextRef = contextRef => this.setState({contextRef})
   render() {
-    const {events, loading} = this.props;
-    if(loading) return <LoadingComponent inverted="true" />
+    const { loading, activities } = this.props;
+    const { moreEvents, loadedEvents } = this.state;
+
+    if (this.state.loadingInitial) return <LoadingComponent inverted={true} />;
+
     return (
-        <Grid>
+      <Grid>
         <Grid.Column width={10}>
-         <EventList deleteEvent = {this.handleDeleteEvent} events={events} />
+          <div ref={this.handleContextRef}>
+            <EventList
+              loading={loading}
+              moreEvents={moreEvents}
+              events={loadedEvents}
+              getNextEvents={this.getNextEvents}
+            />
+          </div>
         </Grid.Column>
         <Grid.Column width={6}>
-          <EventActivity />
+          <EventActivity activities={activities} contextRef={this.state.contextRef} />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Loader active={loading} />
         </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
 
